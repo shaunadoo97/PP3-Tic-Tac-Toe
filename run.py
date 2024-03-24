3196d0725da405e6c872f20b27ca16904b838be4
Shauna Hathaway McHugh <shaunadoo97@gmail.com>
2024-03-24 18:10:25 +0000

    Drawing out the board

--- run.py
+++ run.py
@@ -1,3 +1,22 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+"""
+Pritning the Board
+"""
+
+theBoard = {'1': ' ' , '2': ' ' , '3': ' ' ,
+            '4': ' ' , '5': ' ' , '6': ' ' ,
+            '7': ' ' , '8': ' ' , '9': ' ' }
+
+def get_Board(board):
+    print(board['1'] + '|' + board['2'] + '|' + board['3'])
+    print ('-+-+-')
+    print (board['4'] + '|' + board['5'] + '|' + board['6'])
+    print ('-+-+-')
+    print (board['7'] + '|' + board['8'] + '|' + board['9'])
+
+
+
+
+def main():
+    get_Board
+
