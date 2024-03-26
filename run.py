14109b1f1f436a033a8bd152076ff55dea3b2b5e
Shauna Hathaway McHugh <shaunadoo97@gmail.com>
2024-03-26 18:02:04 +0000

    Computer's turn updated

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 
 import os
+import random
 
 board = ["-", "-", "-", 
         "-", "-", "-",
@@ -98,6 +99,16 @@ def switchPlayer():
         else:
            currentPlayer == "X"
 
+"""
+Computer's Turn
+"""
+def computer(board):
+        while currentPlayer == "O":
+            position = random.randint(0, 8)
+            if board[position] == "-":
+                board[position] = "O"
+                switchPlayer()
+
 
 while gameRunning:
         printBoard(board)
@@ -105,3 +116,6 @@ while gameRunning:
         checkWin(board)
         checkTie(board)
         switchPlayer()
+        computer(board)
+        checkWin()
+        checkTie(board)
