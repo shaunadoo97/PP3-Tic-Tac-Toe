ea281e5f4136e55449eaf9412d994459a6d4b063
Shauna Hathaway McHugh <shaunadoo97@gmail.com>
2024-03-26 17:24:43 +0000

    Updating the col,row and diagnols

--- run.py
+++ run.py
@@ -9,6 +9,9 @@ currentPlayer = "X"
 winner = None 
 gameRunning = True
 
+def resetScreen():
+        os.system("cls" if os.name == "nt" else "clear")
+
 # Drawing out the board
 
 def printBoard(board):
@@ -17,4 +20,47 @@ def printBoard(board):
      print(board[3] + " | " + board[4] + " | " + board[5])
      print("---------")
      print(board[6] + " | " + board[7] + " | " + board[8])
-printBoard()
+printBoard(board)
+
+# Using Int to ensure the number is an integer
+def playerInput(board):
+        inp = int(input("Please enter a number between 1 and 9:"))
+        if inp >= 1 and inp <= 9 and board[inp-1] == "-":
+                board[inp-1] = currentPlayer
+        else: 
+           print("Sorry, this player has already chosen this spot!")
+
+"""
+Checking for win or tie
+"""
+def checkHorizontal(board):
+        global winner
+        if board[0] == board[1] == board[2] and board[1] != "-":
+                winner = board[0]
+                return True
+        elif board[3] == board[4] == board[5] and board[3] != "-":
+                winner = board[3]
+                return True 
+        elif board[6] == board[7] == board[8] and board[6] != "-":
+                winner = board[6]
+                return True 
+
+def checkRow(board):
+        global winner
+        if board[0] == board[3] == board[6] and board[0] != "-":
+                winner = board[0]
+                return True
+        elif board[1] == board[4] == board[7] and board[1] != "-":
+                winner = board[1]
+                return True
+        elif  board[2] == board[5] == board[8] and board[2] != "-":
+                winner = board[2]
+                return True
+
+def checkDiag(board):
+        global winner 
+        if 
+
+while gameRunning:
+        printBoard(board)
+        playerInput(board)
