ecebf32c8c791eb4f174857b767c105b0c9fa61d
Shauna Hathaway McHugh <shaunadoo97@gmail.com>
2024-04-09 07:59:06 +0000

    testing functionalty again

--- run.py
+++ run.py
@@ -11,12 +11,9 @@ WINNER = None
 GAMERUNNING = True
 
 
-# Credits to Code Coach as mentioned in README.
-# This project is also altered to my liking afterwards.
+# Drawing out the board
 
 def printBoard(board):
-    """Drawing out the board for the game
-    """
     print(board[0] + " | " + board[1] + " | " + board[2])
     print("---------")
     print(board[3] + " | " + board[4] + " | " + board[5])
@@ -24,12 +21,12 @@ def printBoard(board):
     print(board[6] + " | " + board[7] + " | " + board[8])
 
 
+    
 def player_input(board):
     """Takes the player input and checking if it is Valid.
     Run through different possibilities for invalid input.
     Place choice on board if valid
     """
-
     while True:
         inp = input(Fore.YELLOW + "Please enter a number between 1 and 9:" + Fore.RESET)
 
@@ -44,7 +41,7 @@ def player_input(board):
     # If it is valid, check that the space is free to choose on the board.
         elif board[int(inp) - 1] != "-":
             print(Fore.RED + "Sorry, a player has already chosen this spot!")
-
+            
     # If all is well with the world, place it and break the loop.
         else:
             board[int(inp) - 1] = CURPLAYER
@@ -82,7 +79,6 @@ def checkRow(board):
 
 
 def checkDiag(board):
-
     global WINNER
     if board[0] == board[4] == board[8] and board[0] != "-":
         WINNER = board[0]
@@ -99,33 +95,22 @@ def checkTie(board):
     global GAMERUNNING
     if "-" not in board:
         printBoard(board)
-        print(Fore.RED + "It's a tie!")
+        print("It's a tie!")
         GAMERUNNING = False
-
-# Again Credits to Code Coach still.
 
 
 def checkWin(board):
     """
-    Checking for Win from either player or computer
+    Checking for Win
     """
     global GAMERUNNING
-    if checkHorizontal(board):
-        printBoard(board)
-        print(f"The winner is {WINNER}!")
-        print(f"Thanks for playing!")
-        GAMERUNNING = False
+    if checkDiag(board) or checkHorizontal(board) or checkRow(board):
+        print(f"The Winner is {WINNER}!")
+        print(f"Thanks for playing.")
+        return True
+    else:
+        return False
 
-    elif checkRow(board):
-        printBoard(board)
-        print(f"The winner is {WINNER}!")
-        print(f"Thanks for playing!")
-        GAMERUNNING = False
-
-    elif checkDiag(board):
-        printBoard(board)
-        print(f"The winner is {WINNER}!")
-        print(f"Thanks for playing!")
         GAMERUNNING = False
 
 
@@ -144,15 +129,13 @@ def computer(board):
     """
     Computer's Turn
     """
+
+    
     while CURPLAYER == "O":
         position = random.randint(0, 8)
         if board[position] == "-":
             board[position] = "O"
             switchPlayer()
-
-# Using if/break to stop the while loop when a player wins.
-
-
 while GAMERUNNING:
         printBoard(board)
         player_input(board)
